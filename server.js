c884187f91399419ab5bf0ab127037d75ad3d8a7
Mohit Sojitra <mohitsojitra0@gmail.com>
2020-06-23 23:20:42 +0530

    add done

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
- return res.send('pong');
+ return res.send('ping pong mohit');
 });
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
