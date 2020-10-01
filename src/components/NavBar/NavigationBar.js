ebc1120d298a4085e594c26acff6bce8ad0ef825
vmnbhuvani7 <31824451+vmnbhuvani7@users.noreply.github.com>
2020-10-01 13:50:17 +0530

    Update NavigationBar.js

--- src/components/NavBar/NavigationBar.js
+++ src/components/NavBar/NavigationBar.js
@@ -11,25 +11,14 @@ import {
 import "./NavigationBar.css";
 import {Link} from "react-router-dom"
 
-// import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-// import App from "../../App.js"
-
 function NavigationBar() {
     let location = window.location
-    
-
     const [isOpen,
         setIsOpen] = useState(false);
-    
     const [activeLink , setActiveLink] = useState("");
-
-    
-
     const toggle = () => setIsOpen(!isOpen);
-
     return (
         <div className="navBar1">
-
             <Navbar color="dark" dark className="fixed-top" expand="md">
                 <NavItem ><Link to="/" className="text-white">JSOM</Link></NavItem>
                 <NavbarToggler onClick={toggle}/>
@@ -42,37 +31,22 @@ function NavigationBar() {
                 }}>
                    
                     <NavLink className="col-md-6 text-right" to="/">
-                    
                             {!isNaN(location.pathname.split("/")[1]) ? "Authors" : location.pathname.split("/")[1]}
-                    
                     </NavLink>
-                    
-
                     <Nav className="ml-auto text-right col-md-4" navbar>
-                    
                         <NavItem className={location.pathname.split("/")[1] === "MostLikedPost" ? "d-none" : ""}>
-                            {/* <NavLink to="/MostLikedPost"> */}
-                            
                                 <Link  to="/MostLikedPost">
                                    <p className="text-secondary m-2"> MostLikedPost</p>
                                 </Link>
-                           
-                            {/* </NavLink> */}
                         </NavItem>
-                        
                         <NavItem className={location.pathname.split("/")[1] === "MostCommentPost" ? "d-none" : ""}>
-                            {/* <NavLink id="2s"> */}
                             <Link  to="/MostCommentPost">
                                    <p className="text-secondary m-2"> MostCommentPost</p>
                             </Link>
-                            {/* </NavLink>   */}
                         </NavItem>
-
                     </Nav>
-
                 </Collapse>
             </Navbar>
-
         </div>
     );
 }
